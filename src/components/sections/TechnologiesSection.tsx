29f07e53b950ee7c3bea70042f0be324cfd0a189
Dawood_Hussain <127896325+dawood90999@users.noreply.github.com>
2025-08-27 03:42:14 +0500

    Design Changes v1.0 release

--- src/components/sections/TechnologiesSection.tsx
+++ src/components/sections/TechnologiesSection.tsx
@@ -169,7 +169,7 @@ const TechnologiesSection = () => {
   const tabsRef = useRef<HTMLDivElement>(null);
   const techGridRef = useRef<HTMLDivElement>(null);
 
-  {/*Ver 1 */}
+  {/*Ver 1.0 */}
   useEffect(() => {
     const ctx = gsap.context(() => {
       // Set initial states
